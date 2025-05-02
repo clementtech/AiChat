a25273ccf74f05dd455fed0097f59977ad693084
clementtech <119944151+clementtech@users.noreply.github.com>
2025-05-02 14:22:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,34 +4,41 @@ try:
     from google import genai
     import sys
 
+        
+    load_dotenv()
+    API_KEY = os.getenv("API_KEY")
+
+
+
+    client = genai.Client(api_key=API_KEY)
+
+    username = str(input("Username: ")).title()
+
+    character = str(input("Who do you want to chat with?: ")).title()
+    print(f"Hello {username}, welcome to {character} chat!")
+    print("Type 'stop' to end the chat.")
+    print("AI response may not be accurate, please do not share any private information.")
+
+
+
+    while True:
+        content = str(input(f"{username}: "))
+
+        if content.lower() == "stop":
+            sys.exit(f"Goodbye {username}, have a great day!")
+
+        elif content == "":
+            raise ValueError
+
+        response = client.models.generate_content(
+            model="gemini-2.0-flash",
+            contents = f"You are a {character}, please be kind and do not mention anything private: {content}"
+        )
+
+        print(f"{character}: {response.text}")
+
 except ModuleNotFoundError:
-    sys.exit("...")
-    
-load_dotenv()
-API_KEY = os.getenv("API_KEY")
+    sys.exit("Please install the required packages by running 'pip install -r requirements.txt'")
 
-
-
-client = genai.Client(api_key=API_KEY)
-
-username = str(input("Username: ")).title()
-
-character = str(input("Who do you want to chat with?: ")).title()
-print(f"Hello {username}, welcome to {character} chat!")
-print("Type 'stop' to end the chat.")
-print("AI response may not be accurate, please do not share any private information.")
-
-
-
-while True:
-    content = str(input(f"{username}: "))
-
-    if content.lower() == "stop":
-        sys.exit(f"Goodbye {username}, have a great day!")
-
-    response = client.models.generate_content(
-        model="gemini-2.0-flash",
-        contents = f"You are a {character}, please be kind and do not mention anything private: {content}"
-    )
-
-    print(f"{character}: {response.text}")
+except ValueError:
+    sys.exit("Invalid Prompt")
