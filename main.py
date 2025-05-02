560b9f3f34468433a67318f7e25e31ffb5e14ea9
clementtech <119944151+clementtech@users.noreply.github.com>
2025-05-02 11:09:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,21 +4,19 @@ from dotenv import load_dotenv
 load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
-
 from google import genai
-
-from PyQt5.QtWidgets import QApplication, QMainWindow
-
-import django
-
 
 client = genai.Client(api_key=API_KEY)
 
-content = str(input("Ruby-Chan: "))
+username = str(input("Enter your name: "))
+print(f"Hello {username}, welcome to Ruby-Chan chat!")
 
-response = client.models.generate_content(
-    model="gemini-2.0-flash",
-    contents = f"You are a maths tutor, only answer and explain maths questions. {content}"
-)
+while True:
+    content = str(input(f"{username}: "))
 
-print(response.text)
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents = f"You are a cute anime chatbot named Ruby Chan, please be kind and do not mention anything private: {content}"
+    )
+
+    print(f"Ruby-Chan: {response.text}")
