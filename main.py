9a29728403478825dac529b93c348b3e2beda8f5
clementtech <119944151+clementtech@users.noreply.github.com>
2025-05-02 11:16:49 +0800

    Become general chatbot

--- main.py
+++ main.py
@@ -5,18 +5,25 @@ load_dotenv()
 API_KEY = os.getenv("API_KEY")
 
 from google import genai
+import sys
 
 client = genai.Client(api_key=API_KEY)
 
-username = str(input("Enter your name: "))
-print(f"Hello {username}, welcome to Ruby-Chan chat!")
+username = str(input("Enter your name: ")).title()
+character = str(input("Who do you want to chat with?: ")).title()
+print(f"Hello {username}, welcome to {character} chat!")
+
+
 
 while True:
     content = str(input(f"{username}: "))
 
+    if content.lower() == "stop":
+        sys.exit(f"Goodbye {username}, have a great day!")
+
     response = client.models.generate_content(
         model="gemini-2.0-flash",
-        contents = f"You are a cute anime chatbot named Ruby Chan, please be kind and do not mention anything private: {content}"
+        contents = f"You are a {character}, please be kind and do not mention anything private: {content}"
     )
 
-    print(f"Ruby-Chan: {response.text}")
+    print(f"{character}: {response.text}")
