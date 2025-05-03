8a11e5247a2796e7641432f8167de0c3810b1882
clementtech <119944151+clementtech@users.noreply.github.com>
2025-05-03 09:59:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,39 @@
-try:
-    import os
-    from dotenv import load_dotenv
-    from google import genai
-    import sys
 
-        
-    load_dotenv()
-    API_KEY = os.getenv("API_KEY")
+import os
+from dotenv import load_dotenv
+from google import genai
+import sys
+
+    
+load_dotenv()
+API_KEY = os.getenv("API_KEY")
 
 
 
-    client = genai.Client(api_key=API_KEY)
+client = genai.Client(api_key=API_KEY)
 
-    username = str(input("Name: ")).title()
+username = str(input("Name: ")).title()
 
-    character = str(input("Who do you want to chat with?: ")).title()
+character = str(input("Who do you want to chat with?: ")).title()
 
-    print(f"Hello {username}, welcome to {character} chat!")
-    print("\n")
-    print("Type 'stop()' to end the chat.")
-    print("AI response may not be accurate, please do not share any private information.")
-    print("\n")
+print(f"Hello {username}, welcome to {character} chat!")
+print("\n")
+print("Type 'stop()' to end the chat.")
+print("AI response may not be accurate, please do not share any private information.")
+print("\n")
 
-    while True:
-        content = str(input(f"{username}: "))
+while True:
+    content = str(input(f"{username}: "))
 
-        if content.lower() == "stop()":
-            sys.exit(f"Goodbye {username}, have a great day!")
+    if content.lower() == "stop()":
+        sys.exit(f"Goodbye {username}, have a great day!")
 
-        elif content == "":
-            raise ValueError
+    elif content == "":
+        raise ValueError
 
-        response = client.models.generate_content(
-            model="gemini-2.0-flash",
-            contents = f"You are a {character}, please be kind and do not mention anything private: {content}"
-        )
+    response = client.models.generate_content(
+        model="gemini-2.0-flash",
+        contents = f"You are a {character}, please be kind and do not mention anything private: {content}"
+    )
 
-        print(f"{character}: {response.text}")
-
-except ModuleNotFoundError:
-    sys.exit("Please install the required packages by running 'pip install -r requirements.txt'")
-
-except ValueError:
-    sys.exit("Invalid Prompt")
+    print(f"{character}: {response.text}")
